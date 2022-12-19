5073ae954858827f2330efd24aee9069e62822f4
Sam Trenholme <git@samiam.org>
2022-12-18 18:29:56 -0800

    regenerate.bat: Now works in Windows

--- regenerate.bat
+++ regenerate.bat
@@ -1,12 +1,11 @@
-#!/bin/sh
-
-# This shell script will regenerate the block hash file so that 
-# allow_block_hash_zero_key does not need to be set to load porn.bin
+rem This batch file will regenerate the block hash file so that 
+rem allow_block_hash_zero_key does not need to be set to load porn.bin
 
 blockHashRead.exe --dump porn.bin > foo.txt
 del foo.bin
 type foo.txt | blockHashMake.exe foo.bin
-ren foo.bin porn.bin
+del porn.bin
+rename foo.bin porn.bin
 del foo.txt
 
 echo porn.bin file regenerated with secure key
